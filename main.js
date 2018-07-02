fe3059d7b9b4a3054df7182998c3432afaa1d9ca
Haniel Diaz <gilastomper@gmail.com>
2018-07-01 23:33:17 -0400

    Added google map after logging in

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function Cancel(url){
   window.location.href = url;
 }
+
+function Login(url){
+  window.location.href = url;
+}
+
+
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 28.6024, lng: -81.2001};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
