5d9731f579a8ea3e3af7c9e6c9b85f7e3ac1b137
Alace95 <32142712+Alace95@users.noreply.github.com>
2018-07-12 15:45:36 -0400

    Add files via upload
    working createacc 7/12

--- createaccount.php
+++ createaccount.php
@@ -22,7 +22,7 @@ if ($result && mysqli_num_rows($result) > 0) {
     printf("Error: That email already exists\n");
     exit();
 }
-      $sql = "INSERT INTO users(password, fname, lname, email) VALUES ('$mypassword', '$myfname', '$mylname', '$myemail')";
+      $sql = "INSERT INTO users(password, fname, lname, email, userLevel) VALUES ('$mypassword', '$myfname', '$mylname', '$myemail', 1)";
       $result = mysqli_query($db,$sql);
       if (!$result) {
     printf("Error: %s\n", mysqli_error($db));
@@ -33,3 +33,54 @@ if ($result && mysqli_num_rows($result) > 0) {
 		
    }
 ?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Create Account</title>
+    <link rel="stylesheet" type="text/css" href="css/large.css" media="screen and (min-width: 1000px)">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src='main.js'></script>
+      </head>
+  <body class="">
+        <div style="margin-top: 10%;"class="CreateAccount CreateAccount-Shadow">
+          <form action="CreateAccount.php" method="post">
+           <div class="container">
+              <div class="row">
+                <div class="col-half">
+                  <input type="text" class="form-textbox" placeholder="First Name" name="fname" required>
+                  <div class="underline"></div>
+                </div>
+                <div class="col-half">
+                  <input type="text" class="form-textbox" placeholder="Last Name" name="lname" required>
+                  <div class="underline"></div>
+                </div>
+              </div>
+              <div class="row">
+                <input type="text" class="form-textbox" placeholder="Email" name="email" required>
+                <div class="underline"></div>
+              </div>
+              <div class="row">
+               <input type="password" class="form-textbox" placeholder="Password" name="psw" required>
+               <div class="underline"></div>
+              </div>
+              <div class="row col-full">
+               <input type="password" class="form-textbox" placeholder="Re-enter Password" name="psw-repeat" required>
+               <div class="underline"></div>
+              </div>
+              <div class="row">
+                <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+              </div>
+             <div class="row">
+               <div class="col-one-third">
+                 <button onclick="Cancel('home.php')" type="button" class="boxshadow CreateAccount-Button">Cancel</button>
+               </div>
+               <div class="col-one-third" style="float: right;">
+                 <button type="submit" style="float: right; margin-right: 12px;" class="boxshadow CreateAccount-Button">Sign Up</button>
+               </div>
+             </div>
+           </div>
+          </form> 
+        </div>
+  </body>
+</html>
